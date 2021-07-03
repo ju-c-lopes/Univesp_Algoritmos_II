5b363c24974e0336d66f89aa1edc0eb24d0eeb74
Juliano <juliano.co.lopes@gmail.com>
2021-07-02 21:04:28 -0300

    finish subject with test class

---
+++ Semana8/teste.py
@@ -0,0 +1,32 @@
+"""
+from func import soma, mult
+
+
+def test_soma():
+    assert soma([1, 2, 3]) == 6, "Deve ser 6"
+
+
+def test_mult():
+    assert mult((2, 3, 4)) == 24, "Deve ser 24"
+
+
+if __name__ == "__main__":
+    test_soma()
+    test_mult()
+    print("Tudo ok!")
+"""
+
+import unittest
+from func import soma, mult
+
+
+class TestSum(unittest.TestCase):
+    def test_sum1(self):
+        self.assertEqual(soma([1, 2, 3]), 6, "Deve ser 6")
+
+    def test_sum2(self):
+        self.assertEqual(mult((2, 3, 4)), 24, "Deve ser 24")
+
+
+if __name__ == "__main__":
+    unittest.main()
